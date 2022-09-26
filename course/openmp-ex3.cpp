5d49eb5f58c7a873eab5a61af4b21665abc9181b
okaya <kayaogz@gmail.com>
2022-09-26 15:35:43 +0200

    course slides + example

---
+++ course/openmp-ex3.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include "omp.h"
+
+int x;
+#pragma omp threadprivate(x)
+
+int main()
+{
+//  int x = 3;
+//  printf("%d %x\n", x, &x);
+#pragma omp parallel num_threads(2)
+  {
+    x = omp_get_thread_num() + 1000;
+    printf("thid = %d, %d %x\n", omp_get_thread_num(), x, &x);
+  }
+#pragma omp parallel num_threads(2) 
+  {
+    printf("thid = %d, %d %x\n", omp_get_thread_num(), x, &x);
+  }
+  return 0;
+}
