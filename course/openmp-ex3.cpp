ebca5f0efbb8f457a370cf42fcc6c003cd5f1f61
okaya <kayaogz@gmail.com>
2022-10-03 14:16:18 +0200

    simd

--- course/openmp-ex3.cpp
+++ course/openmp-ex3.cpp
@@ -6,10 +6,11 @@ int x;
 
 int main()
 {
-//  int x = 3;
+  int x = 3;
 //  printf("%d %x\n", x, &x);
 #pragma omp parallel num_threads(2)
   {
+    int x;
     x = omp_get_thread_num() + 1000;
     printf("thid = %d, %d %x\n", omp_get_thread_num(), x, &x);
   }
